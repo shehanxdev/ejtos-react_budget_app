611ff3f6121eb77d80c7b94f6562cea9c4ec4e24
shehan <shehanxdev@gmail.com>
2023-01-02 13:08:44 -0500

    Initial commit by Shehan

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// Add code to import the components
+import Budget from "./components/Budget";
+import RemainingBudget from "./components/Remaining";
+import ExpenseItem from "./components/ExpenseItem";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseTotal from "./components/ExpenseTotal";
+import AllocationForm from "./components/AllocationForm";
 import { AppProvider } from './context/AppContext';
 const App = () => {
     return (
         <AppProvider>
-            <div className='container'>
-                {/* Add the code here to add the components          */}
+                        <div className='container'>
+                <h1 className='mt-3'>Company's Budget Allocation</h1>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    <div className='col-sm'>
+                        <RemainingBudget />
+                    </div>
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm/>
+                    </div>
+                </div>
             </div>
         </AppProvider>
     );
